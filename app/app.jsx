10a9a4792adddc87c2dc9c5f4c49cee72d3f2f70
Come <come.huguies@algoo.fr>
2017-01-11 18:18:10 +0100

    removed bootstrap and fontawesome from build + fix dependencies

--- app/app.jsx
+++ app/app.jsx
@@ -9,9 +9,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
-import 'bootstrap/dist/css/bootstrap.css'
 require('./css/style.styl')
-require('./lib/font-awesome-4.7.0/css/font-awesome.css')
 
 import Modal from './containers/Modal.jsx'
 
